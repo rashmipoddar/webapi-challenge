b24f05ced4c6c2d5466d548a17f325c8a14c0d49
Rashmi <poddar.rashmi@gmail.com>
2019-11-01 13:01:49 -0400

    Added endpoints for viewing and creating actions for a project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,4 +1,5 @@
 const projectsDb = require('../data/helpers/projectModel');
+const actionsDb = require('../data/helpers/actionModel');
 
 const router = require('express').Router();
 
@@ -95,6 +96,66 @@ router.delete('/:id', (req, res) => {
     console.log(error);
     res.status(500).send({message: 'The project could not be deleted'});
   })
+});
+
+router.get('/:id/actions', (req, res) => {
+  const projectId = req.params.id;
+  // console.log(projectId);
+
+  projectsDb.get(projectId)
+    .then(response => {
+      // console.log(response);
+      if (response) {
+        projectsDb.getProjectActions(projectId)
+          .then(actionsResponse => {
+            res.status(200).send(actionsResponse);
+          })
+          .catch(error => {
+            console.log(error);
+            res.status(500).send({message: 'There was a problem in getting projects actions from the database'});
+          })
+      } else {
+        res.status(404).send({message: 'Project with provided id does not exist'});
+      }   
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({message: 'There was a problem in getting projects actions from the database'});
+    })
+});
+
+router.post('/:id/actions', (req, res) => {
+  const actions = req.body;
+  // console.log(actions);
+
+  const projectId = req.params.id;
+  // console.log(projectId);
+
+  if (!actions.notes || !actions.description) {
+    res.status(400).send({message: 'Action description and notes required'});
+  } else {
+    projectsDb.get(projectId)
+      .then(response => {
+        // console.log(response);
+        if (response) {
+          actionsDb.insert({...req.body, project_id:projectId})
+            .then(actionResponse => {
+              // console.log(actionResponse);
+              res.status(201).send(actionResponse);
+            })
+            .catch(error => {
+              console.log(error);
+              res.status(500).send({message: 'The action could not be created'});
+            })
+        } else {
+          res.status(404).send({message: 'Project with provided id does not exist'});
+        }   
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send({message: 'There was a problem in getting projects from the database'});
+      })
+  }
 });
 
 module.exports = router;
